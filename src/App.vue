51eed2c91b65d2a0cff7ef96ccb6201d05124446
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2021-11-23 09:58:05 +0100

    v1.0

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<header>
+  <Title/>
+</header>
+</template>
+
+<script>
+ import Title from "@/components/Title.vue";
+ export default {
+   name:"App",
+   components: {
+     Title,
+   }
+ } 
+</script>
+
+<style lang="scss">
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20vh;
+}
+</style>
